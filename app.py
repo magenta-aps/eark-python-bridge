498b772dd280be6f2fda53f2984aa48253843ecf
Gunnar H. Heinesen <gunnar@magenta.dk>
2016-07-06 15:43:09 +0200

    Added configuration.

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ import views
 
 if __name__ == '__main__':
     app = flask.Flask(__name__)
+    # Add settings here
+    app.config.update(
+        DEBUG=True,
+    )
     app.add_url_rule('/', view_func=views.IndexView.as_view('index'))
     app.run()
