095633eaa3893280a35219ade04fe09e5f00e8d0
Andreas Kring <andreas@magenta.dk>
2016-12-01 14:56:15 +0100

    New path_to_dict routine done, but not yet implemented in REST

--- sandbox/tree.py
+++ sandbox/tree.py
@@ -1,33 +1,51 @@
 import os
 import os.path
+from pprint import pprint
+# path = '/pre-SIP/representations/rep1/data/dmjx-guide/documentLibrary/Love'
+# prefix_path = '/home/andreas/eark/dmjx'
 
-path = '/pre-SIP/representations/rep1/data/dmjx-guide/documentLibrary/Love'
-prefix_path = '/home/andreas/eark/dmjx'
+path = '/testip/representations/rep1/data/c/g'
+prefix_path = '/home/andreas/eark/eark-python-bridge/sandbox'
 
-folders = path.split('/')[1:]
 
-current_path = prefix_path
-current_dict = {'name': os.path.basename(current_path), 'path': current_path}
-for folder in folders:
-    print 25*'#'
-    print folder
-    
-    current_path = os.path.join(current_path, folder)
-    print current_path
+def gettree(path, prefix_path):
+    folders = path.split('/')[1:]
+    print 'folders =', folders
     
     
-    folders_in_current_path = [f for f in os.listdir(current_path) if os.path.isdir(os.path.join(current_path, f))]
-    if not len(folders_in_current_path) == 0:
-        d = None
-        children = []
-        for f in folders_in_current_path:
-            name_path_dict = {'name': f, 'path': os.path.join(current_path, f)}
-            if f == 
-            children.append(name_path_dict)
+    current_path = os.path.join(prefix_path, folders[0])
+    tree = {'name': os.path.basename(current_path), 'path': current_path}
+    current_dict = tree
+    for i in range(len(folders)):
+        print 25*'#'
+        print folders[i]
         
+        # current_path = os.path.join(current_path, folders[i])
+        print current_path
+        
+        
+        folders_in_current_path = [f for f in os.listdir(current_path) if os.path.isdir(os.path.join(current_path, f))]
+        if not len(folders_in_current_path) == 0:
+    
+            children = []
+            for f in folders_in_current_path:
+                name_path_dict = {'name': f, 'path': os.path.join(current_path, f)}
+                print name_path_dict
+                if f == folders[i + 1]:
+                    d = name_path_dict 
+                children.append(name_path_dict)
+            current_dict['children'] = children
+            current_dict = d
+            print 'current dict =', current_dict
+            current_path = os.path.join(current_path, d['name'])
+    
+    return tree
+
+result = gettree(path, prefix_path)
+pprint(result) 
 
 
-    raise
+    # raise
 """ 
 current_path = root
 current = {name: root, path: root path}   
